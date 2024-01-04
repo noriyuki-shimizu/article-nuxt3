9b469678183da91ac0a615c4948f0f4ed14275e9
清水紀行 <noriyuki.shimizu0612@gmail.com>
2024-01-04 12:08:20 +0900

    dayjs の拡張

--- plugins/dayjs.ts
+++ plugins/dayjs.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs'
+import ja from 'dayjs/locale/ja'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 
@@ -9,6 +10,9 @@ export default defineNuxtPlugin(() => {
   // eslint-disable-next-line import/no-named-as-default-member
   dayjs.extend(timezone)
   dayjs.tz.setDefault('Asia/Tokyo')
+
+  // eslint-disable-next-line import/no-named-as-default-member
+  dayjs.locale(ja)
 
   return {
     provide: {
