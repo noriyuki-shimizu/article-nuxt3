f8f13331b15dc321d4662995c4ac0b9c3d1f3cd1
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-01-06 22:49:59 +0900

    メインアイコンを変更

--- components/layout-contents/LayoutHeader/index.vue
+++ components/layout-contents/LayoutHeader/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import BookIconSvg from '@/assets/svg/book-icon.svg?component'
 import SearchForm from '@/components/projects/SearchForm/index.vue'
 </script>
 
 <template>
   <header :class="$style['header']">
     <NuxtLink to="/" :class="$style['header__link']">
-      <NuxtImg alt="main-icon" src="/img/main-icon.png" width="24" height="24" />
+      <BookIconSvg :class="$style['header__icon']" />
       <h3 :class="$style['header__link-text']">
         技術記事閲覧サイト
       </h3>
