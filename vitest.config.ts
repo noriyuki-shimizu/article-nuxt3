7447cdc71a2283cd0baa89ee6fcaa465ea541ea2
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-01-27 17:01:47 +0900

    テストの設定ファイルを追加

---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { configDefaults } from 'vitest/config'
+
+/** Vitest設定 */
+export default defineVitestConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), '.')
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    include: ['**/*.spec.ts'],
+    exclude: [...configDefaults.exclude],
+    typecheck: {
+      tsconfig: './tsconfig.vitest.json'
+    },
+    coverage: {
+      reporter: ['text-summary', 'lcov'],
+      all: true,
+      enabled: true,
+      include: ['**/*.ts', '**/*.vue'],
+      exclude: [
+        ...(configDefaults.coverage.exclude || []),
+        '*.ts',
+        '*.vue',
+        '**/index.ts',
+        '**/types.ts',
+        '**/constants.ts',
+        '**/enums.ts',
+        'types/**/*.ts',
+        'constants/**/*.ts'
+      ]
+    }
+  }
+})
