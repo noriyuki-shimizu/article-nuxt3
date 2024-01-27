345a75a395e57065b4811f223eb22ea1eb98dc83
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-01-27 19:54:06 +0900

    適切なクラス名に変更

--- components/ui-parts/data-display/ListDetail/index.vue
+++ components/ui-parts/data-display/ListDetail/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="$style['list-item']">
-    <header :class="$style['list-item__header']">
+  <div :class="$style['list-detail']">
+    <header :class="$style['list-detail__header']">
       <slot name="header" />
     </header>
-    <h2 :class="$style['list-item__title']">
+    <h2 :class="$style['list-detail__title']">
       <slot name="title" />
     </h2>
-    <footer :class="$style['list-item__footer']">
+    <footer :class="$style['list-detail__footer']">
       <slot name="footer" />
     </footer>
   </div>
