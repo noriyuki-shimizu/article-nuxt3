bcf67372a1f0e2b5391b6dbd3a24f9a73e0c2659
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-01-31 23:41:41 +0900

    ui-parts にビジネス要件が含まれていたため、修正

--- components/ui-parts/data-display/Empty/index.vue
+++ components/ui-parts/data-display/Empty/index.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import EmptyIconSvg from '@/assets/svg/empty.svg?component'
+</script>
+
 <template>
-  <span>記事情報が見つかりませんでした。。。</span>
+  <div :class="$style['empty']">
+    <slot name="image">
+      <EmptyIconSvg />
+    </slot>
+    <span :class="$style['empty__description']">
+      <slot name="description">No data</slot>
+    </span>
+  </div>
 </template>
+
+<style module lang="scss">
+@use './style.scss';
+</style>
